4258801680665b75d9182dc0d553301b7e9839f4
Prince <Prince@LAPTOP-KFIKPE1S>
2020-07-18 08:26:03 +0530

    First commit

---
+++ 13.Boot-ConfigureJettyServerOptionUsingApplicationProperties/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping("/jetty")
+	public String hello() {
+		return "Jetty Server Configuration";
+	}
+
+}
