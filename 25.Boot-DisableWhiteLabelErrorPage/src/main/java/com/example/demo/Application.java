3f6a453b258e53ca7030e9b8e31a153551551a1c
Prince <Prince@LAPTOP-KFIKPE1S>
2020-07-18 08:39:49 +0530

    First commit

---
+++ 25.Boot-DisableWhiteLabelErrorPage/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication
+// This is Third Way
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
