d846f97cbaf9f655ed3fc9176f344c44a7f82048
Prince <Prince@LAPTOP-KFIKPE1S>
2020-07-18 08:32:10 +0530

    First commit

---
+++ 19.Boot-EnableShedularWithInitialDelay/src/main/java/com/example/demo/delay/Schedular.java
@@ -0,0 +1,20 @@
+package com.example.demo.delay;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class Schedular {
+
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+	
+	@Scheduled(fixedDelay = 5000, initialDelay = 2000)
+	public void reportCurrentTime() {
+		System.err.println("Current Time :"+DATE_FORMAT.format(new Date()));
+	}
+}
